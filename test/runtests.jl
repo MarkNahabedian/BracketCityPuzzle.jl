1243b621c1cb44e79cdc6e9fe190aa47213be696
MarkNahabedian <naha@mit.edu>
2025-09-12 08:56:32 -0400

    Tests to make sure the tokenizer includes trailing text.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,12 @@
 using BracketCityPuzzle
-using BracketCityPuzzle: PuzzleElement
+using BracketCityPuzzle: PuzzleElement, tokenize_puzzle
 using Test
 
 @testset "BracketCityPuzzle.jl" begin
     let
         text = "This [present was] a test."
+        t = tokenize_puzzle(text)
+        @test length(t) == 5
         p = parse_puzzle(text)
         @test length(p) == 3
         @test to_string(p) == text
@@ -17,6 +19,7 @@ using Test
     let
         text = "This [pre[the message was ___ and received] of was] a test."
         p = parse_puzzle(text)
+        @test p[end] == " a test."
         @test to_string(p) == text
         @test preduce(p) == p
         only(findBracket("message", p)).answer = "sent"
@@ -24,8 +27,7 @@ using Test
         @test preduce(p) == PuzzleElement["This is a test."]
         a = all_brackets(p)
         @test length(a) == 2
-        ids = map(b -> b.uid, a)
-        @test ids .- ids[end] == 1:-1:0
+        @test map(b -> b.uid, a) == 1:2
     end
 end
 
