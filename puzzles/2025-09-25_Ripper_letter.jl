589ad7b71271a3a996094bc01fecf87c69b59aba
MarkNahabedian <naha@mit.edu>
2025-09-25 15:25:56 -0400

    today's puzzle

---
+++ puzzles/2025-09-25_Ripper_letter.jl
@@ -0,0 +1,33 @@
+
+
+
+using BracketCityPuzzle
+
+puzzle_text_20250925 = """the "[[[[man who people are always trying to [the first one is the deepest] out] initial for C[Twain protagonist [dorsal ➡️  ⬅️ de siècle]n, familiarly] Cheese or Wile Co[🪀 ➗ 2]te], e.g.] [non-mo[egg (Christmas-y drink 🎄)]amous kind of relation["a[officially [one on a list opposite a con]hibit]don !" 🛟]]er?] ["be your ["at your  risk" ⚠️] " (illusory promise of various business gurus)]" letter is sent"""
+
+parsed = parse_puzzle(puzzle_text_20250925)
+
+show_puzzle(parsed)
+
+set_answer(parsed, 7, "fin")
+set_answer(parsed, 6, "Huck")
+set_answer(parsed, 10, "nog")
+set_answer(parsed, 13, "pro")
+set_answer(parsed, 12, "ban")
+set_answer(parsed, 11, "ship")
+set_answer(parsed, 15, "risk")
+set_answer(parsed, 3, "E")          # Chuck E Cheese, Wile E Coyote
+set_answer(parsed, 4, "middle")     # middle initial
+set_answer(parsed, 8, "yo")         # co yo-yo te
+set_answer(parsed, 5, "cut")        # cut out the middle man
+set_answer(parsed, 14, "boss")
+set_answer(parsed, 9, "open")
+set_answer(parsed, 2, "letter")
+set_answer(parsed, 1, "dear")
+set_answer(parsed, 
+set_answer(parsed, 
+set_answer(parsed, 
+set_answer(parsed, 
+set_answer(parsed, 
+set_answer(parsed, 
+set_answer(parsed, 
