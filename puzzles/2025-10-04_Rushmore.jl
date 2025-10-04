a3b0169f7565c5d9685b1ab38e611614a09c85d6
MarkNahabedian <naha@mit.edu>
2025-10-04 09:18:15 -0400

    today's puzzle.

---
+++ puzzles/2025-10-04_Rushmore.jl
@@ -0,0 +1,25 @@
+using BracketCityPuzzle
+
+puzzle_text_2025_10_04 = """Gutzon Borglum begins sculpting Mount [🌉 [lepre["-[to [word before a ["-wow" says the dog] or a [🙄 give me a ➡️  ⬅️ [ up (energize a clock or [something a cruel, much-superior opp[square number that means the beginning]nt might do with you 🧸🪀🎁] or a toddler)] 💨] or a chance] it on the  is to endure unpleasant events 🥊]g!" 🤑🎰🤑]un's potful ☘️] ➡️  ⬅️ hour 🚗🚙🚕🚙]["hit me [kind of talk that is characterized by a "sing [a theme one should be catchy]" pattern of intonation, higher pitch, slower ["it's a figure of " you are told as your eyes [pointy pub projectile] toward the closet full of [key to every door 🦴]s] rate, and shorter utterances 🍼] one  time" 🎶]"""
+
+parsed = parse_puzzle(puzzle_text_2025_10_04)
+
+show_puzzle(preduce(parsed))
+
+set_answer(parsed, 6, "bow")
+set_answer(parsed, 13, "song")
+set_answer(parsed, 15, "dart")
+set_answer(parsed, 10, "one")          # oppONEnt
+set_answer(parsed, 9, "toy")
+set_answer(parsed, 8, "wind")
+set_answer(parsed, 7, "break")
+set_answer(parsed, 5, "take")
+set_answer(parsed, 4, "chin")
+set_answer(parsed, 3, "chuan")         # lepreCHAun
+set_answer(parsed, 2, "gold")
+set_answer(parsed, 1, "rush")
+set_answer(parsed, 11, "more")         # Mt. rushMORE
+set_answer(parsed, 16, "skeleton")
+set_answer(parsed, 14, "speech")
+set_answer(parsed, 12, "baby")
+
