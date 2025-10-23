7a113f3aedeafe83ef4ae89f528908eb5b4eadac
MarkNahabedian <naha@mit.edu>
2025-10-23 08:35:48 -0400

    more puzzles.

---
+++ puzzles/2025-10-03.jl
@@ -0,0 +1,26 @@
+using BracketCityPuzzle
+
+puzzle_text_2025_10_03 = """“The Guin[[what [who didn't start the fire 🎶] call "we" when [prefix with merge, a[["not a  from you!" (small unit of noise)]ing ➡️  ⬅️foolery]ic, or [[it might be [alternative to better in some wedding vows] than their bite], but deeper maybe]er]stituted for "I" 👑] suffix with high] Book of [[one might be [" night" 🦌 ([composer Jo[Solo described as a "tough James Dean style s[large waterproof sheet that is usually blue for some reason]ilot" by George Lucas]n Sebastian]elor party in the UK)]ed 🎭]er with a [not just want]le]s” debuts"""
+
+parsed = parse_puzzle(puzzle_text_2025_10_03)
+
+show_puzzle(preduce(parsed))
+
+set_answer(parsed, 6, "peep")
+set_answer(parsed, 9, "worse")
+set_answer(parsed, 8, "bark")
+set_answer(parsed, 15, "tarp")
+set_answer(parsed, 14, "Han")
+set_answer(parsed, 13, "bach")
+set_answer(parsed, 12, "stag")
+set_answer(parsed, 16, "need")
+set_answer(parsed, 5, "tom")
+set_answer(parsed, 10, "record")     # Guiness Book Of Records
+set_answer(parsed, 1, "ness")        # Guiness Book Of Records
+set_answer(parsed, 11, "play")
+set_answer(parsed, 4, "sub")         # SUBatomic, SUBmerge    -> SUBstituted
+set_answer(parsed, 3, "we")
+set_answer(parsed, 7, "woof")
+set_answer(parsed, 2, "royal")
+
+show_puzzle(preduce(parsed))
