49ff02bc68233de035542fb90ea1ca6960bad4b0
MarkNahabedian <naha@mit.edu>
2025-11-10 11:24:19 -0500

    More puzzles.

---
+++ puzzles/2025-09-05_beard_tax.jl
@@ -0,0 +1,26 @@
+using BracketCityPuzzle
+
+puzzle = Puzzle("2025-09-05",
+                raw"""Peter the [adjective for [water [orange-ish part of a cigarette] brand]in 🇬🇧 or Gats[preposition with hook or [Nixon ➡️ "I am not a "] in a famous expression] 💰] imposes a [one whose [kind of [word after car or sea 🤢] you might get on a long journey] was invaded by [its purity is measured in ka[Pack that featured Dean [Luther or [one of four of thermo[ duo (e.g. Batman and [one mustn't do this to Peter to pay [John, , George, Ringo]]in)]s]rence or [circuit that can cause a fire 🔥]] and Sammy Davis Jr. 🪤]s]ilocks]d tax in Russia""")
+
+show_puzzle(preduce(puzzle))
+
+set_answer(puzzle, 16, "short")
+set_answer(puzzle, 15, "Paul")
+set_answer(puzzle, 14, "rob")
+set_answer(puzzle, 8, "sick")
+set_answer(puzzle, 13, "dynamic")
+set_answer(puzzle, 5, "crook")
+set_answer(puzzle, 4, "by")
+set_answer(puzzle, 3, "filter")
+set_answer(puzzle, 2, "brita")
+set_answer(puzzle, 1, "great")
+set_answer(puzzle, 12, "law")
+set_answer(puzzle, 11, "martin")
+set_answer(puzzle, 10, "rat")
+set_answer(puzzle, 9, "gold")          # GOLDilocks
+set_answer(puzzle, 7, "home")
+set_answer(puzzle, 6, "bear")
+
+show_puzzle(preduce(puzzle))
+
---
+++ puzzles/2025-09-06_SpadeFork.jl
@@ -0,0 +1,26 @@
+using BracketCityPuzzle
+
+puzzle = Puzzle("2025-09-06",
+                raw"""Exercise [[one might be [cow's exclamation]t or [disappear suddenly, often "without a [[document containing instructions]ly copy using translu[one of 25 in a quarter] paper]"]ing or [credit sought by overa[Ho  Minh City 🇻🇳]evers] 🏈]y black [your birth[kind of d[500 sheets of [word with wall or sand or news 📰]] you have while awake] one is generally not appropriate to wear]] [utensil to the left of the p["better  than [much much later alternative to now]"]] begins""")
+
+show_puzzle(preduce(puzzle))
+
+set_answer(puzzle, 3, "moo")
+set_answer(puzzle, 6, "manual")
+set_answer(puzzle, 7, "cent")
+set_answer(puzzle, 5, "trace")
+set_answer(puzzle, 4, "vanish")
+set_answer(puzzle, 13, "paper")
+set_answer(puzzle, 12, "ream")
+set_answer(puzzle, 9, "chi")
+set_answer(puzzle, 8, "extra")
+set_answer(puzzle, 2, "point")
+set_answer(puzzle, 11, "day")
+set_answer(puzzle, 10, "suit")
+set_answer(puzzle, 16, "never")
+set_answer(puzzle, 15, "late")
+set_answer(puzzle, 14, "fork")
+set_answer(puzzle, 1, "spade")
+
+show_puzzle(preduce(puzzle))
+
