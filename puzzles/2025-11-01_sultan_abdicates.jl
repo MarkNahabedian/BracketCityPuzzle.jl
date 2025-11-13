d420b7bae766c8130df4720c2d838dfe621c8982
MarkNahabedian <naha@mit.edu>
2025-11-13 15:38:08 -0500

    2 more puzzles.

---
+++ puzzles/2025-11-01_sultan_abdicates.jl
@@ -0,0 +1,26 @@
+using BracketCityPuzzle
+
+puzzle = Puzzle("2025-11-01",
+                raw"""The last sul[not [the ori[cot[a metric one is also known as a "me[re[dis[ground with a slide] your guns 💪] sometimes [a sine is a sign you might be in this class, briefly]gered at the dentist]ram"] ➡️  ⬅️ and tonic]al one involved a 🐍] or cos but] of the Ot[the full version of this name is missing has]an [ [something of emergency or of [something made up or bogg[ as[increasingly rare [vol[you might regret opening one of worms]ic output less exciting than lava] depository 🚬]]]] Building] abdicates""")
+
+show_puzzle(preduce(puzzle))
+
+set_answer(puzzle, 16, "can")
+set_answer(puzzle, 7, "pllay")
+set_answer(puzzle, 8, "trig")
+set_answer(puzzle, 6, "flex")
+set_answer(puzzle, 5, "gag")
+set_answer(puzzle, 4, "ton")
+set_answer(puzzle, 3, "gin")
+set_answer(puzzle, 2, "sin")
+set_answer(puzzle, 1, "tan")
+set_answer(puzzle, 9, "tom")        # ThOMhas, otTOMan
+set_answer(puzzle, 10, "empire")
+set_answer(puzzle, 11, "state")     # emire SDTATE building
+set_answer(puzzle, 15, "ash")       # bought the word
+set_answer(puzzle, 14, "tray")
+set_answer(puzzle, 13, "led")
+set_answer(puzzle, 12, "mind")      # bought the first letter.
+
+show_puzzle(preduce(puzzle))
+
