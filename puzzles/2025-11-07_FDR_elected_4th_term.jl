c1e54def888775887e5a41b449f995d36591e939
MarkNahabedian <naha@mit.edu>
2025-11-14 15:42:51 -0500

    Another puzzle.

---
+++ puzzles/2025-11-07_FDR_elected_4th_term.jl
@@ -0,0 +1,26 @@
+using BracketCityPuzzle
+
+puzzle = Puzzle("2025-11-07",
+                raw"""F[st[spirit or character of a [[word after [audibly chortle in a text mess[number on a birthday card] (allegedly)]ly or Iggy 🍾] ➡️  ⬅️ [word before jock or [soak up 🧽]er ⚡️]]]cope wearer, for short 🩺] is elected to a [[writing on this might help you [the S in WYSIWYG (W = "w[location for a magician's rabbit 🐇]", Y = "[[ as a clam] birthday to who?]") 👀] what's coming] Zack Morris used to break on "[" yourself!"]d by the [the Liberty one is cracked 🫑]"] term as US president""")
+
+show_puzzle(preduce(puzzle))
+
+set_answer(puzzle, 16, "be;;")
+set_answer(puzzle, 14, "happy")
+set_answer(puzzle, 12, "hat")
+set_answer(puzzle, 6, "age")
+set_answer(puzzle, 13, "you")
+set_answer(puzzle, 11, "see")
+set_answer(puzzle, 5, "lol")
+set_answer(puzzle, 4, "pop")
+set_answer(puzzle, 15, "save")
+set_answer(puzzle, 10, "wall")
+set_answer(puzzle, 9, "fourth")
+set_answer(puzzle, 1, "dr")
+set_answer(puzzle, 2, "ethos")          # stETHOscope
+set_answer(puzzle, 8, "absorb")         # bought the first letter
+set_answer(puzzle, 7, "shock")
+set_answer(puzzle, 3, "culture")        # bought the word.  How embarassing.
+
+show_puzzle(preduce(puzzle))
+
