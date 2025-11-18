f22f8cd009737c87ce4210339a1971aa8205cfb4
MarkNahabedian <naha@mit.edu>
2025-11-18 16:31:19 -0500

    another puzzle.

---
+++ puzzles/2025-11-12_BackToTheFuture.jl
@@ -0,0 +1,26 @@
+using BracketCityPuzzle
+
+puzzle = Puzzle("2025-11-12",
+                raw"""Marty Mc[one t[sheet that catalogues your mis[no good one goes un["the wedding was so beautiful that even the cake was in tiers", e.g.]ished, some say]s]ped by a [ de Milo 😍]ian plant] gets sent [[funny or [Bah! [[ar[ (spasm) + tac ([🍬 br[it's what you can't do with cake you have, too, it is said]h ➡️  ⬅️ condition ✨]) + toe (game)]le that goes before article instead of a]noy 🐞]! 🎄]erus, e.g.] showed by the courageous] to the [young people are often told they have a b[[just barely an A-]º angle] one]""")
+
+show_puzzle(preduce(puzzle))
+
+set_answer(puzzle, 13, "eat")
+set_answer(puzzle, 5, "venus")
+set_answer(puzzle, 4, "pun")
+set_answer(puzzle, 3, "deed")
+set_answer(puzzle, 2, "rap")
+set_answer(puzzle, 1, "fly")
+set_answer(puzzle, 12, "mint")
+set_answer(puzzle, 11, "tic")
+set_answer(puzzle, 16, "90")              # told they have a bRIGHT one
+set_answer(puzzle, 15, "right")
+set_answer(puzzle, 14, "future")
+set_answer(puzzle, 6, "back")             # BACK to the future
+set_answer(puzzle, 8, "hum")              # HUMerous
+set_answer(puzzle, 10, "an")
+set_answer(puzzle, 9, "bug")
+set_answer(puzzle, 7, "bone")
+
+show_puzzle(preduce(puzzle))
+
