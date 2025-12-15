64ef1f259321c695c70ddeabc868cdb15550d0c3
MarkNahabedian <naha@mit.edu>
2025-12-15 10:22:55 -0500

    A dismal puzzle result.

---
+++ puzzles/2025-11-29_coffee_rationing.jl
@@ -0,0 +1,26 @@
+using BracketCityPuzzle
+
+puzzle = Puzzle("2025-11-29",
+                raw"""Cof[you might have paid a late one to [chain associated with Bit[quarter, [ [a [[something ["!" cries the greedy child 🧒]d with a pickaxe] unit for that greedy kid 🎅] one is an option for a lottery winner] 🥟]e, nickel, penny (rip 🪦), e.g.]]buster 📼 💸] [[🔴🟡 [what kids and cops are encouraged to do with the dots] ➡️  ⬅️ "more years!"]:3 or ["[like some whispered [subject About which there is ["a bit " (polite description for a garish outfit)] Ado for Shakespeare]s]" age for a tee[repeatedly pester]er]:9 or ϕ]ning begins""")
+
+show_puzzle(preduce(puzzle))
+
+set_answer(puzzle, 11, "connect")
+set_answer(puzzle, 10, "4")
+set_answer(puzzle, 16, "nag")
+set_answer(puzzle, 2, "block")           # BLOCKchain
+set_answer(puzzle, 3, "coin")            # bitCOIN
+set_answer(puzzle, 4, "dim")             # DIMe
+set_answer(puzzle, 6, "lump")            # LUMP sum
+set_answer(puzzle, 5, "sum")             # dimSUM
+set_answer(puzzle, 15, "much")           # bougth the first letter
+set_answer(puzzle, 14, "nothing")
+set_answer(puzzle, 13, "sweet")
+set_answer(puzzle, 12, "16")
+set_answer(puzzle, 1, "fee")             # late FEE at blockbuster
+set_answer(puzzle, 8, "mine")            # bought the answer
+set_answer(puzzle, 7, "coal")            # bought the answer
+set_answer(puzzle, 9, "ratio")           # bought the first letter
+
+show_puzzle(preduce(puzzle))
+
