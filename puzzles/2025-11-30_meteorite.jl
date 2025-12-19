d7fe49bed87feeea662606f3596ba79f396b0cff
MarkNahabedian <naha@mit.edu>
2025-12-19 10:39:07 -0500

    A well done puzzle.

---
+++ puzzles/2025-11-30_meteorite.jl
@@ -0,0 +1,26 @@
+using BracketCityPuzzle
+
+puzzle = Puzzle("2025-11-30",
+                raw"""Ann Hodges is hit by a [p[📚 narrative ➡️  ⬅️ de [not quite a qu[", like morality, consists of [quickly pull out your gun in the Old West ✍️]ing the [New Yorkers [what a [courage-seeking creature in Oz] might lie in 🦁] ON one, most others IN one] somewhere": G. K. Chesterton]et]mphe 🇫🇷]el]o[Bar Mitzvah or qu[[% _ @ ([word after make or [a ["I'm [where it meets the [yellow brick route for Dorothy et al.] is the true test], you're " (classroom riposte)] one can help with your arts and crafts project while reducing mess] 🚘] KEY STILL STUCK)] letters ending many official corporate names]eañera, e.g.]""")
+
+show_puzzle(preduce(puzzle))
+
+set_answer(puzzle, 16, "road")
+set_answer(puzzle, 15, "rubber")
+set_answer(puzzle, 14, "glue")
+set_answer(puzzle, 13, "stick")
+set_answer(puzzle, 12, "shift")
+set_answer(puzzle, 8, "lion")
+set_answer(puzzle, 7, "wait")
+set_answer(puzzle, 5, "draw")
+set_answer(puzzle, 6, "line")
+set_answer(puzzle, 10, "inc")            # quINCeañera
+set_answer(puzzle, 11, "3")
+set_answer(puzzle, 4, "art")
+set_answer(puzzle, 3, "trio")
+set_answer(puzzle, 2, "arc")
+set_answer(puzzle, 9, "rite")            # meteoRITE
+set_answer(puzzle, 1, "mete")             # METEorite
+
+show_puzzle(preduce(puzzle))
+
