1b0b241565918a3aaf0cc5de96a5a929613d261a
MarkNahabedian <naha@mit.edu>
2025-12-23 08:53:30 -0500

    Another puzzle.

---
+++ puzzles/2025-12-02_Berkeley_tree-sit.jl
@@ -0,0 +1,28 @@
+using BracketCityPuzzle
+
+puzzle = Puzzle("2025-12-02",
+                raw"""[[the Gilded one and the [[place your [animal whose tail hair is used in a $[number after [prediction-bearing [one of too many in some [the place you should exit if you can't handle the heat 🔥]s]ie that is a pinched toroidal shell] 💰 or Indianapolis 🏎️],000+ mattress] can place for you to win if you place a show bet but not if you place a win bet or a place bet] place medal metal 🥉] one were separated by about 3,000 years] of concern for the [what the evasive do around the [surname for [[something lifted by a [bag choice] surgeon] count for a traitor] presidents]]les]9-day [you might be looking for a [Day preceded by a [like a [transparent house containing plants] Giant or some Ranchers] home invasion] one around now]-sit at Berkeley begins""")
+
+show_puzzle(preduce(puzzle))
+
+set_answer(puzzle, 18, "green")
+set_answer(puzzle, 17, "jolly")
+set_answer(puzzle, 9, "kitchen")
+set_answer(puzzle, 8, "cook")
+set_answer(puzzle, 7, "fortune")
+set_answer(puzzle, 6, "500")
+set_answer(puzzle, 5, "horse")
+set_answer(puzzle, 4, "third")
+set_answer(puzzle, 3, "bronze")
+set_answer(puzzle, 2, "age")
+set_answer(puzzle, 14, "plastic")
+set_answer(puzzle, 13, "face")
+set_answer(puzzle, 12, "two")
+set_answer(puzzle, 11, "bush")
+set_answer(puzzle, 10, "beat")
+set_answer(puzzle, 1, "64")              # Bought the first character
+set_answer(puzzle, 15, "tree")           # berkeley 649 day Tree-sit
+set_answer(puzzle, 16, "christmas")
+
+show_puzzle(preduce(puzzle))
+
