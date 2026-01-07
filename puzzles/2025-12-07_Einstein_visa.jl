f24b1818c566c6ff866af0fccaab50e749cbd5cf
MarkNahabedian <naha@mit.edu>
2026-01-07 10:25:21 -0500

    Two more puzzles.

---
+++ puzzles/2025-12-07_Einstein_visa.jl
@@ -0,0 +1,26 @@
+using BracketCityPuzzle
+
+puzzle = Puzzle("2025-12-07",
+                raw"""Ein[[Red Stripe or Blue [[Cheerios variety represented by [Aldrin or [[the big one is full of cells 🚨] that guides mariners ⛴️]year]Bee, with "Nut"] (postnuptial tradition that apparently originated with medieval newlyweds drinking mead for a lunar cycle)]] vessel 🍺] gets an American [alternative to [🔮 [in front of a light source, form a [it might [word after [[[leaf pictured here 🇨🇦]  (pancake essential) 🥞] that is an antitussive 🗣️] or mic 🎤] at something astounding 😲] using one hand’s thumb and index finger while placing the other hand behind it with two raised fingers to suggest ears (dog [smallest unit of doubt ☀️👤]  instructions) 🐶] ➡️  ⬅️piece 🖼️]card]""")
+
+show_puzzle(preduce(puzzle))
+
+set_answer(puzzle, 15, "maple")
+set_answer(puzzle, 14, "syrup")
+set_answer(puzzle, 13, "cough")
+set_answer(puzzle, 12, "drop")
+set_answer(puzzle, 11, "jaw")
+set_answer(puzzle, 4, "honey")           # HONEY nut cherios
+set_answer(puzzle, 16, "shadow")
+set_answer(puzzle, 10, "puppet")
+set_answer(puzzle, 9, "master")
+set_answer(puzzle, 8, "visa")
+set_answer(puzzle, 5, "buzz")
+set_answer(puzzle, 7, "house")           # big HOUSE alludes to jail or pprison
+set_answer(puzzle, 6, "light")
+set_answer(puzzle, 3, "moon")
+set_answer(puzzle, 2, "beer")
+set_answer(puzzle, 1, "stein")
+
+show_puzzle(preduce(puzzle))
+
