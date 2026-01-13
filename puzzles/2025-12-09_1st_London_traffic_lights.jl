90a04fdc5683429f3866a874059b7e9dc0098985
MarkNahabedian <naha@mit.edu>
2026-01-13 13:27:59 -0500

    another puzzle.

---
+++ puzzles/2025-12-09_1st_London_traffic_lights.jl
@@ -0,0 +1,27 @@
+using BracketCityPuzzle
+
+puzzle = Puzzle("2025-12-09",
+                raw"""the first [something to [it's satisfying when dropped (by a D[companion of PB at lunch] perhaps)] with an early de[result of using a comb (e.g. [[one might have a president or a [Bozo or K[out of practice], e.g.] 🤡] frequently invoked by politicians] or side)]ure] lights are installed in [city with an [[deep meditation might cause you to have an experience [🥊 knock ➡️  ⬅️ of this [scope of a certain Wide [something s["I don’t trust atoms—they make up everything", e.g.] by a spider]] 🌎] of it 😵‍💫] part that sounds like a [envelope contents ✉️] that, from my perspective, means another letter, twelve after the first] 🎡]""")
+
+show_puzzle(preduce(puzzle))
+
+set_answer(puzzle, 16, "letter")
+set_answer(puzzle, 3, "j")
+set_answer(puzzle, 8, "rusty")
+set_answer(puzzle, 7, "clown")
+set_answer(puzzle, 15, "pun")            # sPUN by a spider
+set_answer(puzzle, 14, "web")
+set_answer(puzzle, 13, "world")
+set_answer(puzzle, 12, "out")
+set_answer(puzzle, 11, "body")
+set_answer(puzzle, 10, "eye")
+set_answer(puzzle, 9, "London")
+set_answer(puzzle, 6, "class")
+set_answer(puzzle, 5, "middle")
+set_answer(puzzle, 2, "beat")            # bought the first letter
+set_answer(puzzle, 4, "part")            # bought the answer
+set_answer(puzzle, 1, "traffic")         # bought the first letter
+
+show_puzzle(preduce(puzzle))
+
+           
