4784f799170c2fd664de8267f892d4c04bfd9d70
MarkNahabedian <naha@mit.edu>
2026-01-17 11:28:40 -0500

    two more puzzles.

---
+++ puzzles/2025-12-11_FrankSinatraJr_released_by_kidnappers.jl
@@ -0,0 +1,26 @@
+using BracketCityPuzzle
+
+puzzle = Puzzle("2025-12-11",
+                raw"""Frank Sinatra Jr. is released by [exceptionally soft [car compartment named for something rarely found inside these days]s named for baby [caprine [it [the thing perjurers [sometimes it's a good idea to let [low-power state for a [device with a [necessity's relationship to [something you can pa[portable shelter ⛺️]]]board] 💤]ing dogs do this] on 🤥]s for something] for the best ever 🐐]s not children 🧤]nap[ ca[gyro [w[child that might come out when you see a [word after dodge or wrecking] pit] 🍞 (principal household 💰 earner)]]]s""")
+
+show_puzzle(preduce(puzzle))
+
+set_answer(puzzle, 2, "glove")
+set_answer(puzzle, 16, "ball")
+set_answer(puzzle, 15, "inner")
+set_answer(puzzle, 14, "bread")
+set_answer(puzzle, 13, "pita")
+set_answer(puzzle, 11, "tent")
+set_answer(puzzle, 10, "invention")
+set_answer(puzzle, 9, "mother")
+set_answer(puzzle, 8, "computer")
+set_answer(puzzle, 7, "sleep")
+set_answer(puzzle, 6, "lie")
+set_answer(puzzle, 5, "stand")
+set_answer(puzzle, 12, "per")
+set_answer(puzzle, 1, "kid")                # soft gloves made of goat skin
+set_answer(puzzle, 3, "goat")               # the animal that kids are the babys of
+set_answer(puzzle, 4, "acronym")
+
+show_puzzle(preduce(puzzle))
+
---
+++ puzzles/2025-12-12_MonaLisa_recovered.jl
@@ -0,0 +1,26 @@
+using BracketCityPuzzle
+
+puzzle = Puzzle("2025-12-12",
+                raw"""the ["How the [you might be asked to do this and [[really [dangerous moon phase for a lycanth[word after velvet or tight or jump 🪢] 🐺] 🤰] teddy that was named for Teddy Roosevelt who apparently hated being called "Teddy"] it 😑]ch  Christmas!" 🚨🎄🚨]n "Mona [Simp[Billy Ray's relationship to the P[" for the [[[remove from your hand in [tonic's [your ["Be  Re[the Beaufort scale measures  speed 💨]" (rhyming plea from a video rental store)]red one probably also collects Transformers 🤖 🚛]ual companion], Uno, or draw poker]ed metal]er in a city 🌃]!" (order from a bandit)]er Man in a famous song 🎶] or Kudrow]" is recovered from an Italian waiter's hotel room""")
+
+show_puzzle(preduce(puzzle))
+
+set_answer(puzzle, 16, "wind")
+set_answer(puzzle, 15, "kind")
+set_answer(puzzle, 14, "spirit")
+set_answer(puzzle, 13, "gin")
+set_answer(puzzle, 12, "discard")
+set_answer(puzzle, 6, "rope")
+set_answer(puzzle, 5, "full")
+set_answer(puzzle, 11, "scrap")
+set_answer(puzzle, 10, "sky")
+set_answer(puzzle, 9, "reach")
+set_answer(puzzle, 8, "son")
+set_answer(puzzle, 7, "lisa")
+set_answer(puzzle, 4, "stuffed")
+set_answer(puzzle, 3, "bear")
+set_answer(puzzle, 2, "grin")
+set_answer(puzzle, 1, "stole")
+
+show_puzzle(preduce(puzzle))
+
