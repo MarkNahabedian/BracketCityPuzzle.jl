c9cd18e8383c52fcbf1073c9b4af7724df028a13
MarkNahabedian <naha@mit.edu>
2026-02-06 09:47:36 -0500

    Puzzle for 2025-12-31

---
+++ puzzles/2025-12-31_TimesSquare_ball_drop.jl
@@ -0,0 +1,26 @@
+using BracketCityPuzzle
+
+puzzle = Puzzle("2025-12-31",
+                raw"""the first ever [an [not [get  (settle the [achieve a goal 🥅])]!] one is a [epithet for Al [[alternative to a chin ([wake- call ☎️])] hard]ovic]o 🏀] [unit of [c[[place where a [fourth word in "Jingle Bells" 🎶]ball doesn't stand a chance 🔥] for the polite 😈] taken due to [fair- fan ([🎸 ga["all the " (fashionable)] ➡️  ⬅️-Aid 🤕]wagon jumper)] ⛈️]] in Times Square""")
+
+show_puzzle(preduce(puzzle))
+
+set_answer(puzzle, 13, "snow")        # dashing through the SNOW
+set_answer(puzzle, 8, "up")
+set_answer(puzzle, 12, "hell")
+set_answer(puzzle, 11, "heck")
+set_answer(puzzle, 14, "weather")     # fair WWEATHER fan
+set_answer(puzzle, 10, "rain")        # RAIN check
+set_answer(puzzle, 15, "band")        # BAND wagon
+set_answer(puzzle, 9, "drop")
+set_answer(puzzle, 1, "ball")         # BALL drop in times square
+set_answer(puzzle, 7, "pull")
+set_answer(puzzle, 6, "yank")
+set_answer(puzzle, 5, "weird")
+set_answer(puzzle, 4, "score")
+set_answer(puzzle, 3, "even")
+set_answer(puzzle, 2, "odd")
+set_answer(puzzle, 16, "rage")
+
+show_puzzle(preduce(puzzle))
+
