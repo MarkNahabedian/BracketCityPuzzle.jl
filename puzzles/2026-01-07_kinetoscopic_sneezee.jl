3549738e93caa075c31d96a394d39d45bba0784b
MarkNahabedian <naha@mit.edu>
2026-02-16 11:40:52 -0500

    puzzles for 2026-01-07 and 08.

---
+++ puzzles/2026-01-07_kinetoscopic_sneezee.jl
@@ -0,0 +1,24 @@
+using BracketCityPuzzle
+
+puzzle = Puzzle("2026-01-07",
+                raw"""Thomas Edison makes a kinetoscopic film of a [expel [🥰 [🥁 "and Iiiiiiii......will always  youuuuuu" 🎶] is in the ➡️  ⬅️ [icy sport for [encouraging sight at the end of the [carpal  synd[surprising home for the Spanish [there are [like the eldest preteen] of them in a famous program]] 🎹]]ning and Hurricanes ⛸️] 🥅], mucus, and saliva at [[behind in [ [Le James 🏀]co]ham Palace 🇬🇧🍑]p you're warned about on a sign (usually) 👊]s up to [dollars in a [Big [what Zach Morris was Saved By 🛎️] in Britain]jamin 💵]mph 🤧]""")
+
+show_puzzle(preduce(puzzle))
+
+set_answer(puzzle, 9, "12")
+set_answer(puzzle, 16, "bell")
+set_answer(puzzle, 15, "ben")
+set_answer(puzzle, 14, "100")
+set_answer(puzzle, 13, "bron")
+set_answer(puzzle, 12, "bucking")
+set_answer(puzzle, 11, "bum")
+set_answer(puzzle, 10, "speed")
+set_answer(puzzle, 3, "love")
+set_answer(puzzle, 6, "tunnel")        # carpal TUNNEL syndrome
+set_answer(puzzle, 7, "rome")          # carpal TUNNEL syndROME
+set_answer(puzzle, 5, "light")
+set_answer(puzzle, 4, "hockey")
+set_answer(puzzle, 2, "air")
+set_answer(puzzle, 8, "steps")
+set_answer(puzzle, 1, "sneeze")
+
---
+++ puzzles/2026-01-08_base_jump_BurjKhalifa.jl
@@ -0,0 +1,26 @@
+using BracketCityPuzzle
+
+puzzle = Puzzle("2026-01-08",
+                raw"""[one of four on a [a [color for a spendy [🎶 "walking in a  wonderland" 🎶] [much-loved [one of about [card count in a [hot dog 🌭 or lemonade 🍋 retailing structure]ard deck ♠️♥️♣️♦️]17 in a century]day named for the [wunderkind's language 🧠]ic goddess Frigg 📆]] one might be [expensive or soak tea 🧗] ⛷️] ⚾️] jumpers leap from the Burj Kh[Muhammad who said "I figured that if I said it enough, I would convince the world that I really was the [descriptor for the [[like a frustrating [bird whose [most popular nog, by a lot 🥚] means zero] chase 🪿💨]er 🍫 or Kelly ☔️]ration that fought WWII]" 🥊]fa""")
+
+show_puzzle(preduce(puzzle))
+
+set_answer(puzzle, 16, "egg")
+set_answer(puzzle, 8, "stand")
+set_answer(puzzle, 7, "52")
+set_answer(puzzle, 4, "winter")
+set_answer(puzzle, 15, "goose")
+set_answer(puzzle, 14, "wild")
+set_answer(puzzle, 13, "gene")
+set_answer(puzzle, 12, "greatest")
+set_answer(puzzle, 11, "ali")
+set_answer(puzzle, 9, "german")          # bought the first letter
+set_answer(puzzle, 1, "base")            # BASE jumpers, 4 BASEs in baseball
+set_answer(puzzle, 6, "week")            # bought the answer
+set_answer(puzzle, 5, "friday")
+set_answer(puzzle, 3, "black")
+set_answer(puzzle, 10, "steep")          # bought the answer
+set_answer(puzzle, 2, "diamond")
+
+show_puzzle(preduce(puzzle))
+
