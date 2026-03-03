fff54f929ff7c71bba50f8cc5b624b26c9396a0f
MarkNahabedian <naha@mit.edu>
2026-03-03 13:10:36 -0500

    Puzzles for 2026-01-19 to 20.

---
+++ puzzles/2026-01-19_HowardDean_scream.jl
@@ -0,0 +1,27 @@
+using BracketCityPuzzle
+
+puzzle = Puzzle("2026-01-19",
+                raw"""the "[🏍️ [frustrating paper 🖨️ or traffic 🚗🚙🚙 incident]es ➡️  ⬅️ of [word before ["[one might be British 🎸 or of privacy 👀] of the  Snatchers" 👽] or loans 💸]s] ["The " (Munch [it's applied in ["I'm not going to [companion with [[they [word before "[Tae Kwon  🥋] It" 👟 or "For Men" 👨‍🦳➡️👨‍🦰] Want to Have [ fact, e.g. "no number under 1,000 contains the letter [top grade]"] 🎶] that sold more than 100 million records 🇬🇧] and everything ["no more Mr.  Guy!"]] this" (real talk)]s]ing that looks like this 😱)]" """)
+
+show_puzzle(preduce(puzzle))
+
+set_answer(puzzle, 16, "nice")
+set_answer(puzzle, 13, "do")
+set_answer(puzzle, 12, "just")
+set_answer(puzzle, 15, "a")
+set_answer(puzzle, 14, "fun")
+set_answer(puzzle, 11, "girls")
+set_answer(puzzle, 10, "spice")
+set_answer(puzzle, 9, "sugar")
+set_answer(puzzle, 8, "coat")
+set_answer(puzzle, 7, "paint")
+set_answer(puzzle, 6, "scream")
+set_answer(puzzle, 2, "jam")
+set_answer(puzzle, 5, "invasion")
+set_answer(puzzle, 4, "body")
+set_answer(puzzle, 3, "student")       # bought the first letter
+set_answer(puzzle, 1, "dean")
+
+show_puzzle(preduce(puzzle))
+
+
---
+++ puzzles/2026-01-20_Hamilton_premieres.jl
@@ -0,0 +1,28 @@
+using BracketCityPuzzle
+
+puzzle = Puzzle("2026-01-20",
+                raw""" "[[🇫🇷 = "s'il vous plaît", 🇩🇪 = "[[in[rel["at " (tell a [ecstatic final exclamation from an auctioneer]ier to relax)] frustration or radioactive gases (to prevent explosion)], as a word or phrase 🪙] on the block[the [partner of ["back by [t[the "A" in [one beckoned during a sibling alter[Garfield or Tom or Syl[sleeveless garment]er, e.g.]ion 😭😭]A] that comes in flavors like "Frosted Chocolatey Chip Pancake"]ular "]] one was disrupted by the pandemic ⛓️‍💥] ₿]te", 🇪🇸 = " favor"]cine name for the first chim[give a bad re[what you [day of wallet fattening 🤑] per to watch some boxing] 🍳]zee in space 🚀]ilton" premieres at the Public Theater in NYC""")
+
+show_puzzle(preduce(puzzle))
+
+set_answer(puzzle, 18, "pay")
+set_answer(puzzle, 17, "view")
+set_answer(puzzle, 16, "pan")
+set_answer(puzzle, 15, "vest")
+set_answer(puzzle, 14, "cat")
+set_answer(puzzle, 7, "sold")
+set_answer(puzzle, 6, "ease")
+set_answer(puzzle, 5, "vent")
+set_answer(puzzle, 4, "coin")
+set_answer(puzzle, 13, "mom")         # bought the first letter
+set_answer(puzzle, 12, "art")
+set_answer(puzzle, 11, "pop")
+set_answer(puzzle, 10, "demand")
+set_answer(puzzle, 9, "supply")
+set_answer(puzzle, 8, "chain")
+set_answer(puzzle, 3, "bit")
+set_answer(puzzle, 2, "por")
+set_answer(puzzle, 1, "ham")
+
+show_puzzle(preduce(puzzle))
+
