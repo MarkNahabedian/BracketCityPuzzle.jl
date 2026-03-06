b6f8e4001fb28ab7a35e2fff2adac12de595247e
MarkNahabedian <naha@mit.edu>
2026-03-06 13:34:00 -0500

    Puzzle for 2026-01-24.

---
+++ puzzles/2026-01-24_Guam_Japanese_soldier.jl
@@ -0,0 +1,26 @@
+using BracketCityPuzzle
+
+puzzle = Puzzle("2026-01-24",
+                raw"""Shoichi [[off's opposite]o that married John Len[☝️ [[hold one arm out to the side with your forearm bent at a 90º angle, and move the hand or forearm [-to- ([kids love to hear songs on this 🔂]) 🎒] and forth at the [spot for a watch] or elbow 🏄‍♀️] that sounds like 🪧 or ✍️] qua ➡️  ⬅️-alcoholic [brew[one of two at Zer[Damon or G[fish eggs 🐡]ning 🍩] or Vail 🎿] or suds] 🍺] in 1969]i is found in the jungles of Gu[letters after Pan ✈️ or [slam that is [number of numbers with [stooge or king count] letters] runs ⚾️] 🚘 or will.i. 🎤]""")
+
+show_puzzle(preduce(puzzle))
+
+set_answer(puzzle, 16, "3")
+set_answer(puzzle, 15, "4")
+set_answer(puzzle, 14, "grand")
+set_answer(puzzle, 13, "am")
+set_answer(puzzle, 8, "wrist")
+set_answer(puzzle, 7, "repeat")
+set_answer(puzzle, 6, "back")
+set_answer(puzzle, 5, "wave")
+set_answer(puzzle, 12, "roe")
+set_answer(puzzle, 11, "matt")
+set_answer(puzzle, 10, "ski")
+set_answer(puzzle, 9, "beer")
+set_answer(puzzle, 2, "on")
+set_answer(puzzle, 4, "sine")
+set_answer(puzzle, 3, "non")
+set_answer(puzzle, 1, "yoko")
+
+show_puzzle(preduce(puzzle))
+
