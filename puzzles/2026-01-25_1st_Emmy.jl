26957aa1cce0eccb91c3864fe7e5faeb6b316b7d
MarkNahabedian <naha@mit.edu>
2026-03-07 10:17:07 -0500

    Puzzles for 2026-01-25, 26.

---
+++ puzzles/2026-01-25_1st_Emmy.jl
@@ -0,0 +1,26 @@
+using BracketCityPuzzle
+
+puzzle = Puzzle("2026-01-25",
+                raw"""the [cousin whose [palindromic family [Groucho Marx: "I [say no to 🫷or contents of a trash [when repeated, a high-kicking dance 🥫] 🗑️] to join any club that would have me as a " 🥸]] is your aunt 🏅] [in a.k.a. [what the [way to [an actor might dream of their [size of a [joke or baby goat 🐐] who uses the potty 👏👏👏] one ⛓️‍💥] the ice? 🪮]ins might be] Shady][the Way [🍎 [sweater contest [like the head that wears the [word before molding or Jewels], per a Shakespeare misquote 🫩] on the kitsch 🎄] as ➡️  ⬅️ City 🎰]atra did it] Awards""")
+
+show_puzzle(preduce(puzzle))
+
+set_answer(puzzle, 16, "crown")
+set_answer(puzzle, 11, "kid")
+set_answer(puzzle, 5, "can")
+set_answer(puzzle, 4, "refuse")
+set_answer(puzzle, 3, "member")
+set_answer(puzzle, 2, "mom")
+set_answer(puzzle, 15, "heavy")
+set_answer(puzzle, 14, "ugly")
+set_answer(puzzle, 13, "sin")
+set_answer(puzzle, 1, "first")
+set_answer(puzzle, 10, "big")         # bought the first letter
+set_answer(puzzle, 9, "break")
+set_answer(puzzle, 8, "pick")         # emoji of a hair pick
+set_answer(puzzle, 7, "slim")         # SLIM shady
+set_answer(puzzle, 12, "my")
+set_answer(puzzle, 6, "em")           # EMinEM, bougth the first letter
+
+show_puzzle(preduce(puzzle))
+
---
+++ puzzles/2026-01-26_Churchill_prohibition_prescription.jl
@@ -0,0 +1,26 @@
+using BracketCityPuzzle
+
+puzzle = Puzzle("2026-01-26",
+                raw"""Chur[pill your mom should take re: your re[across the boat from [tw[a s[not [🏆 top-➡️  ⬅️ [[[" me impressed" 🌈] for a great shark 🦈] shaving substance] ☕️] 💡] suspicion ✒️] little one, per the song 💫]board ⛵️] card 💊] gets a doctor's note for alcohol during [word amidst [slang for a [one is [four [🪩 [zone that might change when you [take too many steps in basketball 🏀]]s ➡️  ⬅️ dance 🤠]?] ounces] that can follow s to make a cephalopod 💷🐙] and quo]hibition""")
+
+show_puzzle(preduce(puzzle))
+
+set_answer(puzzle, 16, "travel")
+set_answer(puzzle, 15, "time")
+set_answer(puzzle, 14, "square")
+set_answer(puzzle, 9, "color")
+set_answer(puzzle, 8, "white")
+set_answer(puzzle, 7, "cream")
+set_answer(puzzle, 10, "pro")          # alcohol during PROhibition
+set_answer(puzzle, 6, "heavy")
+set_answer(puzzle, 5, "light")
+set_answer(puzzle, 4, "inkling")
+set_answer(puzzle, 3, "star")
+set_answer(puzzle, 2, "port")
+set_answer(puzzle, 1, "chill")         # churCHILL
+set_answer(puzzle, 11, "quid")         # QUID pro quo
+set_answer(puzzle, 12, "pound")
+set_answer(puzzle, 13, "16")
+
+show_puzzle(preduce(puzzle))
+
