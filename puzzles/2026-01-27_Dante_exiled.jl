84f32a99ace5075d78a37e3131133c469e81c10f
MarkNahabedian <naha@mit.edu>
2026-03-13 10:29:52 -0400

    puzzles for 2026-01-27, 28.

---
+++ puzzles/2026-01-27_Dante_exiled.jl
@@ -0,0 +1,26 @@
+using BracketCityPuzzle
+
+puzzle = Puzzle("2026-01-27",
+                raw"""D[initial [what a certain [one ad["you are " 📍]d to [car you in[[fastball or [there might be a line for it at the play[confine a teen to their [ sweet  🏠] as a punishment] 🛝]r, e.g.]ed shelter 🏕️]ionally crash] (e.g. "Honk If Kids Fall Out")] sticks to 🥟] filler ♠️♥️♣️♦️] is exiled from F[Sophia who starred in "[one might be open or ar[🏌️‍♀️ [" Miss [fancy word for s[valley's counterpart 🏔️]er's platform]y" 🚙🌼] ➡️  ⬅️ Rover 🚘]d] Italian Style" 🇮🇹]ce""")
+
+show_puzzle(preduce(puzzle))
+
+set_answer(puzzle, 16, "peak")
+set_answer(puzzle, 15, "dais")
+set_answer(puzzle, 14, "driving")
+set_answer(puzzle, 13, "range")
+set_answer(puzzle, 12, "marriage")
+set_answer(puzzle, 11, "loren")
+set_answer(puzzle, 10, "home")
+set_answer(puzzle, 9, "ground")
+set_answer(puzzle, 8, "slide")
+set_answer(puzzle, 7, "pitch")
+set_answer(puzzle, 6, "tent")
+set_answer(puzzle, 5, "bumper")
+set_answer(puzzle, 4, "here")
+set_answer(puzzle, 3, "sticker")
+set_answer(puzzle, 2, "pot")
+set_answer(puzzle, 1, "ante")            # bought the answer
+
+show_puzzle(preduce(puzzle))
+
---
+++ puzzles/2026-01-28_Bal_des_Ardents.jl
@@ -0,0 +1,26 @@
+using BracketCityPuzzle
+
+puzzle = Puzzle("2026-01-28",
+                raw"""the "B[Jaze["end of an " ⌛️] or R[letters signified by bringing the tip of your [something it's unfair to put on the [rigid, protective [word after home or license 🍽️] for a fish or dinosaur ⚖️]] into contact with the tip of your [alphabetical reference at the back of a book 👆] finger to form a closed [hole beloved by the tax a["null and " 🚫]ant 🎢], and extending the remaining three fingers upwards]er] des Ar[cozy [office that is [square garment for S[early At[short name for [er name for a [🤑 hit it ➡️  ⬅️ Apple 🗽] piano 🎹]e 🎤] video game 🏓]eBob]-optional 🏠] for a 🦁 or a 🐻]ts" """)
+
+show_puzzle(preduce(puzzle))
+
+set_answer(puzzle, 16, "big")
+set_answer(puzzle, 9, "void")
+set_answer(puzzle, 8, "loop")
+set_answer(puzzle, 7, "index")
+set_answer(puzzle, 6, "plate")
+set_answer(puzzle, 5, "scale")
+set_answer(puzzle, 4, "thumb")
+set_answer(puzzle, 3, "ok")
+set_answer(puzzle, 2, "era")
+set_answer(puzzle, 1, "al")
+set_answer(puzzle, 10, "den")
+set_answer(puzzle, 13, "pong")
+set_answer(puzzle, 12, "pants")
+set_answer(puzzle, 14, "ari")        # atARI video game
+set_answer(puzzle, 15, "grand")
+set_answer(puzzle, 11, "home")
+
+show_puzzle(preduce(puzzle))
+
