dd885ea40ff6c7dc5090972997ec524919bddbb0
MarkNahabedian <naha@mit.edu>
2026-03-13 13:44:07 -0400

    Puzzles for 2026-01-29, 30.

---
+++ puzzles/2026-01-29_MorrisFrank_SeeingEye.jl
@@ -0,0 +1,26 @@
+using BracketCityPuzzle
+
+puzzle = Puzzle("2026-01-29",
+                raw"""Morris Frank founds "The [📸 [remedy for [like some losers and most post-[🔪 [sur[Zuck's book 😲] of many unpaved roads]y ➡️  ⬅️ in progress ⚠️]out 🏋️‍♀️] e[answer indicated by a [ [not on] 💤]]?]➡️  ⬅️ is believing 👀] [body [a verb is one of [word after [remnant of a f[suffix with gaz 🦌 or [" in America" 🇺🇸]mois 🇫🇷]d 🌳] or freedom of]] that sounds like [will who marry me? 🫵]r word for you]" """)
+
+show_puzzle(preduce(puzzle))
+
+set_answer(puzzle, 16, "you")
+set_answer(puzzle, 15, "made")
+set_answer(puzzle, 14, "elle")
+set_answer(puzzle, 13, "stump")
+set_answer(puzzle, 6, "face")
+set_answer(puzzle, 5, "dirt")
+set_answer(puzzle, 4, "work")
+set_answer(puzzle, 9, "off")
+set_answer(puzzle, 8, "nod")
+set_answer(puzzle, 7, "yes")
+set_answer(puzzle, 3, "sore")
+set_answer(puzzle, 2, "sight")
+set_answer(puzzle, 1, "seeing")
+set_answer(puzzle, 10, "eye")
+set_answer(puzzle, 12, "speech")
+set_answer(puzzle, 11, "part")
+
+show_puzzle(preduce(puzzle))
+
---
+++ puzzles/2026-01-30_renamed_to_SanFrancisco.jl
@@ -0,0 +1,26 @@
+using BracketCityPuzzle
+
+puzzle = Puzzle("2026-01-30",
+                raw"""Yerba Buena is renamed "S[indefinite ["the genuine " 📰] before "[" of faith" 📰]" instead of a] [[Dr. with [😀 up➡️  ⬅️ to the punch 🥊]s by him 🎧]scher 💁‍♀️💄 or Le[tie for a [names like [[something [bubble [" it up" you say [a wire one almost always requires a warrant 👂]ping your invisible wristwatch 🎁] sound 🍾]ped with a ring❓] or check?] Twain or [one of [like noon or mid[stand next to your bed 🛌]] for a typical car battery ⚡️]aire, e.g. 🖋️]guin suit 🙇‍♂️]itz 👓🚬]cisco" """)
+
+show_puzzle(preduce(puzzle))
+
+set_answer(puzzle, 16, "night")
+set_answer(puzzle, 15, "12")
+set_answer(puzzle, 14, "volt")
+set_answer(puzzle, 2, "article")
+set_answer(puzzle, 13, "tap")
+set_answer(puzzle, 12, "wrap")
+set_answer(puzzle, 11, "pop")
+set_answer(puzzle, 10, "question")
+set_answer(puzzle, 3, "article")
+set_answer(puzzle, 1, "an")
+set_answer(puzzle, 4, "fran")
+set_answer(puzzle, 9, "mark")
+set_answer(puzzle, 8, "pen")
+set_answer(puzzle, 7, "bow")
+set_answer(puzzle, 6, "beat")
+set_answer(puzzle, 5, "dre")
+
+show_puzzle(preduce(puzzle))
+
