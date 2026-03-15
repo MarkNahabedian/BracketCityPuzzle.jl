28242d727b0b806c9169f382cb4fb6f815dac4bf
MarkNahabedian <naha@mit.edu>
2026-03-15 10:31:22 -0400

    Puzzle for 2026-01-31.

---
+++ puzzles/2026-01-31_broadcast_of_1st_daytime_soap_opera.jl
@@ -0,0 +1,26 @@
+using BracketCityPuzzle
+
+puzzle = Puzzle("2026-01-31",
+                raw"""the first daytime [[🎶 [exceed [Big sandwich sold by a clown named Ronald 🤡🍔]h 1 to make a [[azure, ceru[Cuisine for the calorie conscious], navy, etc.] [word before [raiser that might feature a [like the beginning of "[🔪 American ➡️  ⬅️[very up[like some ["show him the " (help the new guy) 🪢] or squeezes]]ysis 🧐🧠]" or "Ghost"?] auction 🤫💸] or maze]hog archrival of Dr. Robotnik 🦔🤖] one 🛩️💨]➡️  ⬅️ [supplies including staplers, [" to self" 🎵]books, pens, etc.] 🎟️] for an orator] opera is broadcast on NBC""")
+
+show_puzzle(preduce(puzzle))
+
+set_answer(puzzle, 7, "lean")
+set_answer(puzzle, 14, "ropes")
+set_answer(puzzle, 16, "note")
+set_answer(puzzle, 15, "office")
+set_answer(puzzle, 13, "tight")
+set_answer(puzzle, 4, "mac")
+set_answer(puzzle, 6, "blue")
+set_answer(puzzle, 12, "anal")
+set_answer(puzzle, 11, "psycho")
+set_answer(puzzle, 10, "silent")
+set_answer(puzzle, 9, "fund")
+set_answer(puzzle, 8, "hedge")
+set_answer(puzzle, 5, "sonic")
+set_answer(puzzle, 3, "boom")
+set_answer(puzzle, 2, "box")
+set_answer(puzzle, 1, "soap")
+
+show_puzzle(preduce(puzzle))
+
