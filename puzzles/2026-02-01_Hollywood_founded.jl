117e30989c5c84b8f8b73328a37f080fa5519191
MarkNahabedian <naha@mit.edu>
2026-03-16 12:55:36 -0400

    Puzzle for 2026-02-01.

---
+++ puzzles/2026-02-01_Hollywood_founded.jl
@@ -0,0 +1,26 @@
+using BracketCityPuzzle
+
+puzzle = Puzzle("2026-02-01",
+                raw"""[🎶 "boughs of" ➡️  ⬅️ [" directly to jail. Do not pass , do not collect $200" ([target of anti[fall into another's [the [planet on which a day is longer than a year] de Milo doesn't have them anymore ⚔️]?] laws] edict) 🎩][how to tread around sensitive [[[kiss given with eyelashes 🧈🪰] who has lots of friends] studies or Spanish]s] 🕶️][traditional [["ugh I have to choose between two [word before [the [the H in HR 🧍‍♀️] one is actually a species 🏇] 📺 or Spider-Man 🎥] job offers" 😫 = a good one to have] for a drain 🪠] material 🇳🇱] is founded as a temperate utopia""")
+
+show_puzzle(preduce(puzzle))
+
+set_answer(puzzle, 16, "human")
+set_answer(puzzle, 2, "go")
+set_answer(puzzle, 6, "venus")
+set_answer(puzzle, 5, "arms")
+set_answer(puzzle, 10, "butterfly")
+set_answer(puzzle, 9, "scial")
+set_answer(puzzle, 4, "trust")
+set_answer(puzzle, 3, "monopoly")
+set_answer(puzzle, 8, "subject")
+set_answer(puzzle, 15, "race")               # bought the first leytter
+set_answer(puzzle, 7, "lightly")             # bought the first leytter
+set_answer(puzzle, 1, "holly")
+set_answer(puzzle, 14, "amazing")            # bought the answer
+set_answer(puzzle, 11, "wood")
+set_answer(puzzle, 12, "clog")
+set_answer(puzzle, 13, "problem" )           # bought the answer
+
+show_puzzle(preduce(puzzle))
+
