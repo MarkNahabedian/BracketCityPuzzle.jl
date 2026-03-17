4decc78383c8a85092a46e7b8509d25c50fd2cb1
MarkNahabedian <naha@mit.edu>
2026-03-17 12:38:16 -0400

    puzzle for 2026-02-02.

---
+++ puzzles/2026-02-02_incorporation_of_NewAmsterdam.jl
@@ -0,0 +1,26 @@
+using BracketCityPuzzle
+
+puzzle = Puzzle("2026-02-02",
+                raw"""[word before Deal 👷 or Testament 📖] Am[letters after lob 🦞 or [oft-glazed Christmas [one of [Black[Skellington 💀 or Sparrow 🏴‍☠️] target 🇺🇸🍸]0 in your average office [[🧱 [with an [w[classic thing to do on your laurels]ler who competes sitting down], a very high p[among the healthiest kind of [it might be ceremoniously cut at a w[Sullivan 🎙️ or [basically see-through (garment) or perpendicular ([hanger that leaves you wanting more 🧗‍♀️])]an ➕✖️➗🟰➖]ding]s 🍚🎂]]o ➡️  ⬅️ in stone 🪨] for kids at the playground]line, filled to capacity ⊓]] 🐹]dam is incorporated""")
+
+show_puzzle(preduce(puzzle))
+
+set_answer(puzzle, 16, "cliff")
+set_answer(puzzle, 11, "rest")
+set_answer(puzzle, 10, "arm")
+set_answer(puzzle, 6, "jack")
+set_answer(puzzle, 1, "new")
+set_answer(puzzle, 5, "21")
+set_answer(puzzle, 15, "sheer")
+set_answer(puzzle, 2, "ster")         # new amSTERdam, lobSTER, hamSTER
+set_answer(puzzle, 3, "ham")          # HAMster
+set_answer(puzzle, 14, "ed")
+set_answer(puzzle, 13, "cake")
+set_answer(puzzle, 12, "rice")
+set_answer(puzzle, 9, "leg")
+set_answer(puzzle, 8, "set")
+set_answer(puzzle, 7, "swing")
+set_answer(puzzle, 4, "staple")
+
+show_puzzle(preduce(puzzle))
+
