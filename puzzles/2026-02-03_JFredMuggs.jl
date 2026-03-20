96dc04b18b028d029ce2e2f1d0060b4c51769d65
MarkNahabedian <naha@mit.edu>
2026-03-20 10:46:31 -0400

    Puzzles for 2026-02-03, 04.

---
+++ puzzles/2026-02-03_JFredMuggs.jl
@@ -0,0 +1,26 @@
+using BracketCityPuzzle
+
+puzzle = Puzzle("2026-02-03",
+                raw"""NBC hires [🪼 PB & ➡️ _ ⬅️ [off-screen companion of [your friends might sign it after a [🍂 from g[💄 Ru[[one in the oven is a [like a very small step toward a goal 🍼]]yan 🪓 or Revere 🏇]'s Drag ➡️  ⬅️ against time ⏱️] or into place] 🩼]] 👔]. Fred [[one in the dark is a wild [" [question it sounds like an owl is asking 🦉]?" you say as you cover your pa[model who you might look up to] officer's e[more [something to [-in-the-blank (laziest clue format)] out]al yep] 🙈]] after a chase? 🚨☕️🚨]gs""")
+
+show_puzzle(preduce(puzzle))
+
+set_answer(puzzle, 13, "role")
+set_answer(puzzle, 12, "who")
+set_answer(puzzle, 16, "fill")
+set_answer(puzzle, 15, "form")
+set_answer(puzzle, 14, "yes")
+set_answer(puzzle, 8, "baby")
+set_answer(puzzle, 7, "bun")
+set_answer(puzzle, 6, "paul")
+set_answer(puzzle, 5, "race")
+set_answer(puzzle, 4, "fall")
+set_answer(puzzle, 3, "cast")
+set_answer(puzzle, 2, "crew")
+set_answer(puzzle, 1, "j")
+set_answer(puzzle, 11, "guess")
+set_answer(puzzle, 10, "shot")
+set_answer(puzzle, 9, "mug")
+
+show_puzzle(preduce(puzzle))
+
---
+++ puzzles/2026-02-04_PattyHearst.jl
@@ -0,0 +1,26 @@
+using BracketCityPuzzle
+
+puzzle = Puzzle("2026-02-04",
+                raw"""Patty H[your ideas for new T[stood for office]s[[es[[arroz  pollo]ference with a lot of sh[openly gay or in t[radiator or [voluntarily not eat]ball 🥵]s]ed questions]o + milk]r's counterpart]s have seemingly fallen on deaf ]t is kidnapped by the Sym[class where you might hear "mitochondria are the [st[insulting term for a tou[capital of Po["o'er the  of the [period without class]" 🇺🇸]?]ee (🏉 - 🐝)]gle depicted in "Succession" 🔌]house of the [mobile 📱]", for short 🧫🧪]nese Liberation Army""")
+
+show_puzzle(preduce(puzzle))
+
+set_answer(puzzle, 6, "con")
+set_answer(puzzle, 15, "free")
+set_answer(puzzle, 14, "land")
+set_answer(puzzle, 2, "ran")
+set_answer(puzzle, 16, "cell")
+set_answer(puzzle, 11, "power")
+set_answer(puzzle, 10, "bio")
+set_answer(puzzle, 12, "rug")
+set_answer(puzzle, 13, "p")
+set_answer(puzzle, 1, "ears")
+set_answer(puzzle, 9, "fast")
+set_answer(puzzle, 8, "heater")
+set_answer(puzzle, 7, "out")
+set_answer(puzzle, 5, "press")
+set_answer(puzzle, 4, "latte")
+set_answer(puzzle, 3, "former")
+
+show_puzzle(preduce(puzzle))
+
