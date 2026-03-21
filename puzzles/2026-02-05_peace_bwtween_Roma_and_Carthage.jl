2af8eba5b30543ba7ca6cb4721450d79bc8ceffb
MarkNahabedian <naha@mit.edu>
2026-03-21 10:53:41 -0400

    Puzzle for 2026-02-05.

---
+++ puzzles/2026-02-05_peace_bwtween_Roma_and_Carthage.jl
@@ -0,0 +1,26 @@
+using BracketCityPuzzle
+
+puzzle = Puzzle("2026-02-05",
+                raw"""[Com often featuring [1/1000 of a [palindromic job for a [class where you might be issued a ["for the " 🔴]er 🎶🪈]ian]abyte, for short] Ryan or [the O in [chromo["MAKE  NOISE!" commands the [especially [size of living that might involve an infinity pool 🤑], as a jet perhaps ✈️]tron] every human has at least one of]O 🤗]h [approve, as a request] 👩‍❤️‍👨]e and [increasingly metaphorical spot for items before [you might seek a sa[something p["ew" factor 💩]ed by the pe[Radcliffe, to his pals 👓🪄]tic]y one before sending an email]out 🛒]hage make peace after 2,131 years""")
+
+show_puzzle(preduce(puzzle))
+
+set_answer(puzzle, 16, "dan")
+set_answer(puzzle, 10, "large")
+set_answer(puzzle, 9, "jumbo")
+set_answer(puzzle, 8, "some")
+set_answer(puzzle, 7, "x")
+set_answer(puzzle, 6, "hug")
+set_answer(puzzle, 2, "meg")
+set_answer(puzzle, 11, "grant")
+set_answer(puzzle, 3, "gig")
+set_answer(puzzle, 1, "rom")
+set_answer(puzzle, 12, "cart")        # CARThage
+set_answer(puzzle, 15, "ick")         # bought the first letter
+set_answer(puzzle, 14, "nit")
+set_answer(puzzle, 13, "check")
+set_answer(puzzle, 5, "record")       # bought the answer
+set_answer(puzzle, 4, "music")
+
+show_puzzle(preduce(puzzle))
+
