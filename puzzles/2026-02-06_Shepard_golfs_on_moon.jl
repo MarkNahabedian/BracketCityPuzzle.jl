2576542d63ccbc385b522f5d844f37acf9c0790f
MarkNahabedian <naha@mit.edu>
2026-04-01 08:59:39 -0400

    puzzle for 2026-02-06.

---
+++ puzzles/2026-02-06_Shepard_golfs_on_moon.jl
@@ -0,0 +1,26 @@
+using BracketCityPuzzle
+
+puzzle = Puzzle("2026-02-06",
+                raw"""Alan Shepard [sport with [[something [monthly expense for tenants]ed or raced]penter's medium]s played by a [animal after Saber-[cash-rich f[kiss that doesn't land 😘 or plane that hopefully does 🛬]y 💰😮]ed 🦷 or [B[Frost [haiku 🇯🇵 or [[one of 88 on a piano]  pie]rick 🇮🇪, e.g.] actually called "The  Not Taken" (not "The  [mini[African country that sounds like [item of interest to [word that starts many races]llum and Sauron 💍]wald or Shannon 🇲🇱]sts say it's more] Traveled") 🛣️]way award rarely awarded on Broadway itself] the 🥣]]s on the moon""")
+
+show_puzzle(preduce(puzzle))
+
+set_answer(puzzle, 16, "go")
+set_answer(puzzle, 15, "ring")
+set_answer(puzzle, 12, "key")
+set_answer(puzzle, 4, "rent")
+set_answer(puzzle, 14, "mali")       # miniMALIsts
+set_answer(puzzle, 13, "less")
+set_answer(puzzle, 3, "car")
+set_answer(puzzle, 2, "wood")
+set_answer(puzzle, 11, "lime")
+set_answer(puzzle, 10, "poem")
+set_answer(puzzle, 9, "road")
+set_answer(puzzle, 8, "tony")
+set_answer(puzzle, 6, "tooth")       # saber-TOOTHed tiger
+set_answer(puzzle, 5, "tiger")
+set_answer(puzzle, 7, "air")         # bought the answer
+set_answer(puzzle, 1, "golf")
+
+show_puzzle(preduce(puzzle))
+
