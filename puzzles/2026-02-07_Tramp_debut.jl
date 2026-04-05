d3676ba4d4616c499004838db9862d0dc2d0c271
MarkNahabedian <naha@mit.edu>
2026-04-05 08:56:42 -0400

    puzzle for 2026-02-07.

---
+++ puzzles/2026-02-07_Tramp_debut.jl
@@ -0,0 +1,26 @@
+using BracketCityPuzzle
+
+puzzle = Puzzle("2026-02-07",
+                raw""" "The [[title for [Ellie the [awkward proboscidean in the [your [pair in [[word after ["!" a.k.a. the last thing you hear before getting [whack [market for a recent college grad 💼👨‍💼]?] in the head with a golf ball] 🌥️ or broad 📻] a line for dinner or compliments][makes after ["death and " (the [child without siblings] certainties per Benjamin Franklin)]]] might get more in an [strategy that might involve an Irish [iconic AOL sign-off word 👋] or a parachute] row]]'s WNBA team, with New York 🗽] or Luck]'s dinner date 🐶+🍝+🐶 = ❤️]" makes his debut""")
+
+show_puzzle(preduce(puzzle))
+
+set_answer(puzzle, 14, "only")
+set_answer(puzzle, 13, "taxes")
+set_answer(puzzle, 11, "job")
+set_answer(puzzle, 10, "hit")
+set_answer(puzzle, 9, "fore")
+set_answer(puzzle, 16, "goodbye")
+set_answer(puzzle, 15, "exit")
+set_answer(puzzle, 12, "nets")          # bought the clue
+set_answer(puzzle, 8, "cast")           # bought the answer
+set_answer(puzzle, 7, "fish")
+set_answer(puzzle, 6, "legs")
+set_answer(puzzle, 5, "room")
+set_answer(puzzle, 4, "elephant")
+set_answer(puzzle, 3, "liberty")
+set_answer(puzzle, 2, "lady")
+set_answer(puzzle, 1, "tramp")
+
+show_puzzle(preduce(puzzle))
+
