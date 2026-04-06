d4f7440c447628bce06bdd748457ff6285ffba48
MarkNahabedian <naha@mit.edu>
2026-04-06 15:48:22 -0400

    Puzzles for 2026-02-10, 11.

---
+++ puzzles/2026-02-10_ducttape_FDR.jl
@@ -0,0 +1,26 @@
+using BracketCityPuzzle
+
+puzzle = Puzzle("2026-02-10",
+                raw"""[garment for people with [common thing you catch 🤧] [[it might be [[kind of scientist who might be working on a [🥱 bored to ➡️  ⬅️ Valley 🥵] ray]e (as a putt) or already spent (as a cost)]en or buried ⚱️👑💎] container]s and warm [scary race for [word before Bowl 🏈 or nova 🌟]powers]]a Stoudt pitches duct [mea[like a [alternative to [you might speak in the [base close to home] one when you are feeling haughty] or place in "[when repeated, like hind["love at first " 😍]] questions"] that is bound to happen] that retracts 📼] to FDR""")
+
+show_puzzle(preduce(puzzle))
+
+set_answer(puzzle, 16, "sight")
+set_answer(puzzle, 14, "third")
+set_answer(puzzle, 13, "person")
+set_answer(puzzle, 9, "super")
+set_answer(puzzle, 8, "arms")
+set_answer(puzzle, 2, "cold")
+set_answer(puzzle, 7, "death")
+set_answer(puzzle, 6, "mad")
+set_answer(puzzle, 5, "sunk")
+set_answer(puzzle, 4, "treasure")
+set_answer(puzzle, 3, "chest")
+set_answer(puzzle, 1, "vest")
+set_answer(puzzle, 10, "tape")
+set_answer(puzzle, 11, "sure")
+set_answer(puzzle, 12, "thing")
+set_answer(puzzle, 15, "20")            # backsolved.  I don't get the clue.
+
+show_puzzle(preduce(puzzle))
+
---
+++ puzzles/2026-02-11_EmperorJimmu.jl
@@ -0,0 +1,28 @@
+using BracketCityPuzzle
+
+puzzle = Puzzle("2026-02-11",
+                raw"""Emperor [😜 [the [important im[💪 bench ➡️  ⬅️ release 🗞️]ions] one was patented by l [-Hur or [[Lab [[[ honcho]s up, you lose (if you [word after booty or prank] this)] ridden by opportunists]?]ter pictured here 🌭]lin]z in 1886]rey or [rooster lover]son 🐸]mu founds [where you might need ["and" en e[place for a [access a [Republic with [something to cut someone who needs a break]s]] 🧖]ñol]en for what you yen for]""")
+
+show_puzzle(preduce(puzzle))
+
+set_answer(puzzle, 18, "slack")
+set_answer(puzzle, 17, "banana")
+set_answer(puzzle, 16, "peel")
+set_answer(puzzle, 4, "press")
+set_answer(puzzle, 10, "head")
+set_answer(puzzle, 11, "call")
+set_answer(puzzle, 3, "first")
+set_answer(puzzle, 9, "tails")
+set_answer(puzzle, 12, "hen")
+set_answer(puzzle, 15, "spa")
+set_answer(puzzle, 14, "y")
+set_answer(puzzle, 13, "japan")
+set_answer(puzzle, 1, "jim")
+set_answer(puzzle, 8, "coat")          # bought the first letter
+set_answer(puzzle, 7, "fur")           # bought the first letter.  I totally don't get the clue
+set_answer(puzzle, 6, "frank")
+set_answer(puzzle, 5, "ben")
+set_answer(puzzle, 2, "car")
+
+show_puzzle(preduce(puzzle))
+
