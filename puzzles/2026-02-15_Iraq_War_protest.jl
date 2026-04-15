8f8b30ab83e41edac264cfc5d5289d6c34d6c4a0
MarkNahabedian <naha@mit.edu>
2026-04-15 07:35:34 -0400

    Puzzle for 2026-02-15.

---
+++ puzzles/2026-02-15_Iraq_War_protest.jl
@@ -0,0 +1,27 @@
+
+using BracketCityPuzzle
+
+puzzle = Puzzle("2026-02-15",
+                raw"""Millions [exercise your 1st Amendment [two [like "[[ to the [make a long [alternative to a [prefix that means after 📫] on Instagram] short?]or] opener?] comes after M"]s don't make one, but three [div[Lord of the Rings [[z II Men (👦👦 ➡️ 👱‍♂️👱‍♂️)] your parents might warn you about][go to a [-to-[word after dining room or [shop with to-go cups]]] upstate, maybe]]ed, say 💔]s do] 🪧] the Iraq War""")
+
+show_puzzle(preduce(puzzle))
+
+set_answer(puzzle, 16, "coffee")
+set_answer(puzzle, 15, "table")
+set_answer(puzzle, 14, "farm")
+set_answer(puzzle, 8, "post")
+set_answer(puzzle, 12, "boy")
+set_answer(puzzle, 7, "story")
+set_answer(puzzle, 11, "bad")
+set_answer(puzzle, 2, "right")
+set_answer(puzzle, 3, "wrong")
+set_answer(puzzle, 9, "left")
+set_answer(puzzle, 13, "die")          # bought the answer
+set_answer(puzzle, 10, "orc")
+set_answer(puzzle, 6, "edit")
+set_answer(puzzle, 5, "letter")
+set_answer(puzzle, 1, "protest")
+set_answer(puzzle, 4, "L")             # Bought the first letter.  I still don't understand the clue.
+
+show_puzzle(preduce(puzzle))
+
